4633ce57372a30456dc2fbdb0f1a9eb85ea40618
Gourav soni <gouravsoni8476@gmail.com>
2026-02-07 19:52:04 +0530

    added hashpass and check pass hash util fn

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,12 @@
 package utils
 
-import "regexp"
+import (
+	"encoding/base64"
+	"regexp"
+
+	"github.com/charmbracelet/log"
+	"golang.org/x/crypto/bcrypt"
+)
 
 var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 
@@ -12,4 +18,38 @@ var phoneRegex = regexp.MustCompile(`^[0-9]{10}$`)
 
 func IsPhoneValid(p string) bool {
 	return phoneRegex.MatchString(p)
+}
+
+func HashPassword(password string) (string, error) {
+
+	decoded, err := base64.StdEncoding.DecodeString(password)
+
+	if err != nil {
+		return "", err
+	}
+
+	bytes, err := bcrypt.GenerateFromPassword(decoded, 14)
+	if err != nil {
+		log.Warn("error in generating password", err)
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	decoded, err := base64.StdEncoding.DecodeString(password)
+	if err != nil {
+		decoded = []byte("justapass")
+	}
+
+	if hash == "" {
+		hash = "$2a$12$WQ/Li.jWLQ74PjWQEm16jOBCQvR80ItyEiBnAtVtrXYIfEYkBO8HG"
+	}
+
+	if err != nil {
+		return false
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hash), decoded)
+	return err == nil
 }
