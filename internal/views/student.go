170a8c738bb30c0bdc89be803df30a8772320f4b
Yash Raj Kohli <yarajkohli@gmail.com>
2026-02-17 22:48:00 +0530

    ##update##

--- internal/views/student.go
+++ internal/views/student.go
@@ -1,8 +1,19 @@
 package views
 
-type StudentSignUp struct { //TODO add more info as required per user
-	Username *string `json:"username"`
-	Password string  `json:"password"`
-	Phone    *string `json:"phone"`
-	Email    *string `json:"email"`
+import "collegeWaleServer/internal/enums/college"
+
+type StudentInfoResponse struct { //TODO add more info as required per user
+	FirstName  string             `json:"first_name"`
+	LastName   string             `json:"last_name"`
+	Email      string             `json:"email"`
+	Phone      string             `json:"phone"`
+	RollNumber string             `json:"roll_number"`
+	CourseType college.CourseType `json:"course_type"`
+	Year       int                `json:"year"`
+	Subjects   []string           `json:"subjects"`
+	//// relationships
+	//CollegeID uint    `gorm:"not null"` // fk -> college
+	//College   College `gorm:"foreignKey:CollegeID"`
+
+	//Subject []Subject `gorm:"many2many:student_subjects"`
 }
